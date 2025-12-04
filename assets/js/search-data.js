9c8b8f6c75e71ab85c69239b9ac7bab43a2b3248
Bingbing-0131 <suyc22@mails.tsinghua.edu.cn>
2025-12-04 10:21:03 +0000

    Deploying to gh-pages from @ Bingbing-0131/Bingbing-0131.github.io@4f0cb90d3004b3bb46e7d6d61ab3e7bfa945a450 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
@@ -53,14 +46,14 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-progressive-sound-synthesis",
+          title: 'Progressive Sound Synthesis',
+          description: "guided by Professor Doug James, Mentored by Eris Zhang and  Kangrui Xue",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project.html";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-computational-design-of-bio-inspired-fliers",
+          title: 'Computational Design of Bio-Inspired Fliers',
+          description: "guided by Professor Tao Du",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project.html";
             },},{id: "projects-project-3-with-very-long-name",
